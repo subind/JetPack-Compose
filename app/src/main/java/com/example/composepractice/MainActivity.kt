fb7d60eddc15bc91fb58a8302e52aefc9829089f
subind214 <subind214@gmail.com>
2022-06-28 13:41:17 +0530

    Row & Column

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +25,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                TextViewHello("Subi")
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .background(color = Color.Red)
+                        .fillMaxSize(0.5f)
+                        .border(5.dp, Color.Black)
+                ) {
+                    Text(text = "first")
+                    Text(text = "Second")
+                    Text(text = "Third")
+                }
             }
         }
     }
 }
 
-@Composable
-fun TextViewHello(name: String) {
-    Text(text = "Hello $name !!!")
-}
 
-@Preview
-    (name = "Preview Name",
-    showBackground = true,
-    showSystemUi = true)
+/*@Preview(showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    ComposePracticeTheme {
-        TextViewHello("Android")
-    }
-}
+fun ComposablePreview() {
+
+}*/
+
